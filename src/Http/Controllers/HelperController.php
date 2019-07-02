a1a9cbba8a00448c34357da7f956460ebf92c7cf
aman <rahmat.adi.putra.21@gmail.com>
2019-07-02 15:44:39 +0700

    library helper

--- src/Http/Controllers/HelperController.php
+++ src/Http/Controllers/HelperController.php
@@ -4,6 +4,7 @@ namespace Msi\Falcon\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
 use Msi\Falcon\Models\Ktv\Song;
+use Illuminate\Support\Facades\File;
 
 class HelperController extends BaseController
 {
@@ -13,7 +14,12 @@ class HelperController extends BaseController
         $song = Song::all();
         foreach ($song as $k => $v) {
             $data['song'][$v['id']] = $v['file_path'];
-        } 
-        return $data['song'];
+        }
+
+        $files = File::files('http://192.168.7.224/');
+        foreach ($files as $file) {
+            $data['files'][] = pathinfo($file);
+        }
+        return $data['files'];
     }
 }
