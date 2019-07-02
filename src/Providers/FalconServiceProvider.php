991a9253cf7b3731d8c5c8ee8c7b7df4b5140d1e
aman <rahmat.adi.putra.21@gmail.com>
2019-07-02 13:47:25 +0700

    library helper

---
+++ src/Providers/FalconServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Msi\Helper\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class HelperServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        
+    }
+}
